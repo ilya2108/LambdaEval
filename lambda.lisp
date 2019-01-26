2a2b495f241e0c3e7757b211a425f4168c4f239f
Ilya Ryabukhin <ryabuily@fit.cvut.cz>
2019-01-26 13:46:34 +0300

    Conditions added

--- lambda.lisp
+++ lambda.lisp
@@ -1,7 +1,22 @@
-(defun subst (x y z)
+(defun mySubst (x y z)
 
   (cond ((atom z)
          (cond ((eq z y) x)
                ('t z)))
-        ('t (cons (subst x y (car z))
-                  (subst x y (cdr z ))))))
+        ('t (cons (mySubst x y (car z))
+                  (mySubst x y (cdr z ))))))
+
+(defun myLength (x)
+	(if(null x)
+		1
+		(+ 1 (myLength (cdr x)))
+	)
+)
+
+(defun myEval(x)
+	(cond
+		((equal (car x) 'zero) (if (equal (cadr x) 0) (caddr x) (cadddr x)))
+	)
+)
+
+
