8daa94c65d3baa4547af7f22f619544f6d0854e6
Ilya Ryabukhin <ryabuily@fit.cvut.cz>
2019-02-10 16:17:24 +0300

    Aplha reduction added

--- lambda.lisp
+++ lambda.lisp
@@ -1,19 +1,18 @@
 
 (defun notBound (args body tmp)
-  (cond
-    ;exit from recursion
-    ((not (car body)) tmp)
-    ;goes recursively through nested lambdas
-    ((listp (car body))
-    	(notBound args (cdr body) (append tmp (list (f args (car body) '()))))
-    ((atom (car body))
-    	(if (member (car body) args) 
-    		;ignore element from list of bounded variables 
-    		(notBound args (cdr body) tmp)
-    		;append to accumulator
-    		(notBound args (cdr body) (append tmp (list (car body))))
+	(cond
+	    ;exit from recursion
+	    ((not (car body)) (remove nil tmp))
+	    ;goes recursively through nested lambdas
+	    ((listp (car body))
+	    	(notBound args (cdr body) (append tmp (list (notBound (append (list (cadr body)) args) (car body) '())))))
+	    ((atom (car body))
+	    	(if (member (car body) args) 
+	    		;ignore element from list of bounded variables 
+	    		(notBound args (cdr body) tmp)
+	    		;append to accumulator
+	    		(notBound args (cdr body) (append tmp (list (car body))))
+			)
 		)
-	))
+	)
 )
-
-
